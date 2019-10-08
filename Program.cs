c493f7e46794b3744471cfe9774c1a3a81d272e9
Johannes Andersson <johannes_hp@hotmail.com>
2019-10-08 22:51:53 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hoja_talet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
